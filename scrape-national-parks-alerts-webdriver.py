ca449db035286352022aa1ce27a708db00b083d9
Adam Marton <amarton@umd.edu>
2022-03-29 16:18:12 -0400

    Update scrape-national-parks-alerts-webdriver.py

--- scrape-national-parks-alerts-webdriver.py
+++ scrape-national-parks-alerts-webdriver.py
@@ -8,6 +8,8 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+
 
 
 #import beautiful soup plus
